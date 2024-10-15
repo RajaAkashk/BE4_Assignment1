ccace905c8e937e87b5e5fbc58f44b6635e44103
Raja Akash Kumar <rajaakashk9@gmail.com>
2024-10-15 10:38:59 +0530

    Added CorsOption

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ app.use(express.json());
 initializeDatabase();
 
 const cors = require("cors");
+const corsOption = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+}
 app.use(cors());
 
-// Add new book to dabase.
+// Add new book to database.
 async function addBookData(newBook) {
   try {
     const newBookToAdd = new Books(newBook);
@@ -245,3 +250,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port:-", PORT);
 });
+
+module.exports = app; 
